73b7eaee19c5044ff83e509279df389bf2eda7fa
Amahasla1 <do.flam1ngo@yandex.ru>
2025-03-04 22:46:32 +0400

    написал кубик

---
+++ Math3D.js
@@ -0,0 +1,17 @@
+class Math3D {
+    constructor ({WIN}) {
+        this.WIN = WIN;
+    }
+    xs (point) {
+        const zs = this.WIN.CENTER.z;
+        const z0 = this.WIN.CAMERA.z;
+        const x0 = this.WIN.CAMERA.x;
+        return (point.x - x0) / (point.z - z0) * (zs - z0) + x0;
+    }
+    ys (point) {
+        const zs = this.WIN.CENTER.z;
+        const z0 = this.WIN.CAMERA.z;
+        const y0 = this.WIN.CAMERA.y;
+        return (point.y - y0) / (point.z - z0) * (zs - z0) + z0;
+    }
+}
